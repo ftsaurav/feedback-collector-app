ca681212db68be586c748f220cb169e3247ffe2b
ayush <ak4426280@gmail.com>
2025-04-10 15:58:01 +0530

    frontend modified

--- frontend/src/assets/api.js
+++ frontend/src/assets/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-const API1= "http://localhost:8080/feedback";
-const API2= "http://localhost:8080/feedbacks";
+const API1= "/feedback";
+const API2= "/feedbacks";
 
 export const getBlogs = () => axios.get(API2);
 export const createBlog = (data) => axios.post(API1, data);
